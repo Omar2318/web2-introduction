b9cf0f618ef7939d5462d4a4f535f095bcc69087
Omar <omar.ramirezm65@gmail.com>
2026-02-25 19:37:42 -0600

    Formulario reactivo

--- src/pages/reactive-forms/reactive-forms.ts
+++ src/pages/reactive-forms/reactive-forms.ts
@@ -1,9 +1,31 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-
+import { JsonPipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 @Component({
   selector: 'app-reactive-forms',
-  imports: [],
+  imports: [ReactiveFormsModule, JsonPipe],
   templateUrl: './reactive-forms.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ReactiveForms { }
+export class ReactiveForms {
+  private fb = inject(FormBuilder);
+  productForm: FormGroup;
+
+  constructor(){
+    this.productForm = this.fb.group({
+      name: [,Validators.required],
+      description: [],
+      price: [,Validators.required],
+      stock: [,Validators.required],
+    });
+  }
+
+  saveProduct() {
+
+    if(this.productForm.invalid) {
+      this.productForm.markAllAsTouched();
+      return;
+    };
+    console.log(this.productForm.value);
+  }
+}
